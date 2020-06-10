6e5984faf66a4cd3135f2994c13e99caf58ccfc2
mairagee524 <garciamaira524@yahoo.com>
2020-06-10 16:13:05 -0700

    added classes and functionality

--- lib - classes and helper code/Intern.js
+++ lib - classes and helper code/Intern.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(school) {
+        this.school = school;
+        this.getSchool = function(){
+
+        }
+        this.getRole = function(){
+            return Intern;
+        }
+    }
+}
